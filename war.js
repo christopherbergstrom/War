91d3cfcedf571d05815aafb397521311e143f0fd
Christopher Bergstrom <christopherrbergstrom@gmail.com>
2016-05-28 00:38:28 -0600

    fixed instructions

--- war.js
+++ war.js
@@ -38,7 +38,7 @@ function createInstructions()
 {
   var instructionsText = document.createElement("div");
   instructionsText.setAttribute("id", "instructionsText");
-  instructionsText.innerHTML="Your deck and card that you play are on the bottom half of the screen. Draw cards to play the game. The player with the higher card value, keeps both cards. If both cards have the same value each card goes back to its original owner. The game ends when one player has all of the cards.";
+  instructionsText.innerHTML="Your deck and card that you play are on the bottom half of the screen. Draw cards to play the game. The player with the higher card value keeps both cards. If both cards have the same value, each card goes back to its original owner. The game ends when one player has all of the cards.";
   body.appendChild(instructionsText);
 }
 function createDeck()
